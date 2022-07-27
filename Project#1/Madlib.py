1cd2e93ef2f99726cb80df12754d4eec9a3f20a4
Bhargavi Teki <77991973+bteki@users.noreply.github.com>
2022-07-27 14:27:47 -0700

    New project

---
+++ Project#1/Madlib.py
@@ -0,0 +1,12 @@
+# string concatenation
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_per = input("Famous Person: ")
+
+
+madlib = f"Computer programming is so {adj} and I love {verb1}. Stay hydrated and {verb2} like you are {famous_per}!"
+
+print(madlib)
