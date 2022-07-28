f3022ae578afc180189614028e5f311fa59cfdf1
Bhargavi Teki <77991973+bteki@users.noreply.github.com>
2022-07-28 14:12:46 -0700

    Completed the guessing game project

---
+++ Project#1/guessing_game.py
@@ -0,0 +1,21 @@
+import random
+
+def guess(x):
+    rand_num = random.randint(1, x)
+    guess = 0
+    while guess != rand_num:
+        guess = int (input(f'Guess a number between 1 and {x}: '))
+        if guess < rand_num:
+            print("Sorry too low, guess again: ")
+        elif guess > rand_num:
+            print("Sorry too high, guess again: ")
+
+    print (f'YAY, you did! you guessed the nummber {rand_num} correctly!!!')\
+
+guess(10)
+
+
+
+
+
+
