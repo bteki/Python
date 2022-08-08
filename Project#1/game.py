38b1768c10976c7ec98fe43ee575d149e4e26dde
Bhargavi Teki <77991973+bteki@users.noreply.github.com>
2022-08-07 20:18:09 -0700

    DS&A day1

--- Project#1/game.py
+++ Project#1/game.py
@@ -1,3 +1,5 @@
+
+'''
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]
@@ -12,3 +14,4 @@ class TicTacToe:
         number_board = [[str(i) for i in range (j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
+'''
